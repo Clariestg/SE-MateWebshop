8eb0aa79fa4e8fc882556f506e4ebae45954c57e
Princess Claries Tuma Gwangwa <claries_tuma@outlook.com>
2025-04-02 10:23:21 +0200

    Grundgerüst des MateWebShops Projekt
    1st Commit

---
+++ MateWebshop/src/main/java/com/example/sematewebshop/SeMateWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.sematewebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeMateWebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeMateWebshopApplication.class, args);
+    }
+
+}
---
+++ MateWebshop/src/test/java/com/example/sematewebshop/SeMateWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sematewebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeMateWebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
